6f75e3caf056ae62c828f20476a439cebd05e426
Matheus Moreira <mthsmoreira59@gmail.com>
2023-12-03 19:46:18 -0300

    muda acesso do id

--- src/models/Like.ts
+++ src/models/Like.ts
@@ -2,7 +2,7 @@ import { User } from "./User";
 import { v4 as uuidv4 } from "uuid";
 
 export class Like {
-    id: string;
+    private readonly id: string;
 
     constructor(public _username: string) {
         this.id = uuidv4();
