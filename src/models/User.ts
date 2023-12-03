feb8da48471f510812b602b93b3024322d5bc11b
Matheus Moreira <mthsmoreira59@gmail.com>
2023-12-03 20:02:11 -0300

    getters User

--- src/models/User.ts
+++ src/models/User.ts
@@ -15,3 +15,49 @@ export class User {
     get username(): string {
         return this._username;
     }
+
+    get email(): string {
+        return this._email;
+    }
+
+    sendTweet(newTweet: Tweet): void {
+        this._tweets.push(newTweet);
+    }
+
+    reply(replyContent: Tweet, repliedTweet: Tweet) {
+        repliedTweet.replies.push(replyContent);
+    }
+
+    showTweets(): void {
+        let likesText: string;
+
+        this._tweets.forEach((tweet, i) => {
+            if (tweet._likes.length === 0) {
+             likesText = `[${tweet._likes.length} curtidas]`
+            } else if (tweet._likes.length === 1) {
+             likesText = `[@${tweet._likes[i]._username} curtiu]`
+            } else if (tweet._likes.length > 1) {
+             likesText = `[@${tweet._likes[1]._username} e mais ${tweet._likes.length - 1} pessoas curtiram isso]`
+            }
+
+            console.log(
+                `@${this._username}: ${tweet.content}\n${likesText}\n > @${tweet.replies[i].username}:${tweet.replies[i].content}\n--------------------`
+            )
+        })
+    }
+
+    follow(user: User): void {
+        followings.push(user);
+        console.log(`Você está seguindo o user ${user._username}`);
+
+    }
+
+    likeTweet(tweet: Tweet, like: Like): void {
+        const hasLiked = tweet._likes.some(liked => liked._username === like._username);
+        if (hasLiked) {
+            console.log("Você já deu like neste tweet!");
+            return;
+        }
+        tweet._likes.push(like);
+    }
+}
