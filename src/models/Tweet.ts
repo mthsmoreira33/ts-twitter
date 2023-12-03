57872d0ae3803427cb3df400cf6e4332c9c7053b
Matheus Moreira <mthsmoreira59@gmail.com>
2023-12-03 19:46:49 -0300

    cria constructor da classe Tweet

--- src/models/Tweet.ts
+++ src/models/Tweet.ts
@@ -3,3 +3,9 @@ import { User } from "./User";
 import { Like } from "./Like";
 
 export class Tweet {
+    private readonly id: string;
+
+    constructor(private _content: string, private _tweetType: 'normal' | 'reply', private _username: string, private _replies: Tweet[] = [], public _likes: Like[] = []) {
+        this.id = uuidv4();
+    }
+
